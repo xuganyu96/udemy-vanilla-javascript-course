48f7281e4f708ed9dc495e686206cc4a936ca456
Bruce Xu <xuganyu@berkeley.edu>
2020-07-16 21:48:13 -0700

    Select single and/or multiple elements

--- sec03-dom-manipulation-and-events/app.js
+++ sec03-dom-manipulation-and-events/app.js
@@ -46,3 +46,69 @@ try{
 // However, you can iterate by converting an HTMLCollection object to an Array
 linksArray = Array.from(document.links)
 linksArray.forEach(function(link){console.log(link.getAttribute('class'));})
+
+/*
+Selecting a single element in a webpage
+*/
+// The easiest way to select an HTML element is by its id attribute
+taskTitle = document.getElementById("task-title");
+console.log(`${typeof taskTitle}'s id is ${taskTitle.id}`);
+// Once an element is selected, we can reassign its attributes to modify how it is displayed
+console.log(`task-title's font weight is originally undeclared`);
+taskTitle.style.fontWeight = 'bold';
+console.log(`task-title's font weight is now ${taskTitle.style.fontWeight}`);
+// One way to make an element disappear is by setting "display" to "none":
+// taskTitle.style.display = 'none';
+// Modify the text content within this tag using innerText attribute
+console.log(`"taskTitle.innerText evaluates to ${taskTitle.innerText}`);
+taskTitle.innerText = "My Tasks"
+console.log(`"taskTitle.innerText is reassigned to ${taskTitle.innerText}`);
+// We can also insert HTML!
+taskTitle.style.display = 'flex';
+taskTitle.style.alignItems = "center";
+taskTitle.innerHTML = "<i class='material-icons'>assignment</i><span>My Tasks</span>";
+// A more powerful way is by using querySelector, which takes in CSS Selector. querySelector is always selecting a 
+// single element, and if there are more than one fitting elements, the first one will be selected
+// Select by ID using a pound-sign 
+console.log(`The query "#task-title" corresponds to ${document.querySelector('#task-title')}`);
+// Select by class using the dot (will return the first element of this class)
+collectionItem = document.querySelector('.collection-item');
+console.log(collectionItem.innerText);
+// Select by tag
+let collectionUL = document.querySelector("ul");
+console.log(collectionUL);
+// To select one among the many using pseudo-classes
+collectionItem2 = document.querySelector('.collection-item:nth-child(2)')
+collectionItem2.innerHTML = 'Selected by <span style="font-family:Monospace;color:blue;">.collection-item:nth-child(2)</span>'
+
+/*
+Selecting multiple elements in a webpage
+*/
+// The more basic way is using functions mentioned below, which returns HTMLCollection objects.
+let collectionItems = document.getElementsByClassName('collection-item');
+console.log(collectionItems);
+collectionItems[0].innerHTML = "Selected by " +
+"<span style=\"font-family:Monospace;color:blue;\">document.getElementsByClassName('collection-item')[0]</span>";
+// Recall 
+// let collectionUL = document.querySelector("ul");
+let collectionLIs = collectionUL.getElementsByTagName('li');
+console.log(`There are ${collectionLIs.length} <li> tags in first <ul> element`)
+// Now the better way: querySelectorAll; querySelectorAll returns a NodeList, which behaves like a list and can have
+// forEach applied to it
+collectionItems = document.querySelectorAll('ul li');
+console.log(collectionItems);
+console.log(`document.querySelectorAll(\'ul li\').length evluates to ${collectionItems.length}`);
+collectionItems.forEach(function(li, index){
+    console.log(li, index, li.innerText);
+})
+// Finally let's do a striping list just for the heck of it:
+let collectionItemEven = document.querySelectorAll('ul li:nth-child(even)');
+for(let itemIndex = 0; itemIndex < collectionItemEven.length; itemIndex++){
+    item = collectionItemEven[itemIndex];
+    item.style.background = "#eee"
+}
+let collectionItemOdd = document.querySelectorAll('ul li:nth-child(odd)');
+console.log("H", collectionItemOdd);
+collectionItemOdd.forEach(function(li){
+    li.style.background="#ccc";
+});
