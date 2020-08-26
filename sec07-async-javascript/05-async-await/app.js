7f4fc63244f63b3f9379bfecbfea2914ace2aac8
Bruce Xu <ganyu.x@leantaas.com>
2020-08-25 18:38:10 -0700

    Getting started with async-await

---
+++ sec07-async-javascript/05-async-await/app.js
@@ -0,0 +1,5 @@
+// async can be applied to a function to make it return a Promise that resolves to the thing that the function will 
+// be returning without the async applied;
+//
+// await can be applied to a Promise that will make the runtime wait for the Promise to be resolved before executing 
+// subsequent code.
